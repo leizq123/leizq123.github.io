4caebf70c805591e864fb744e5d7633b577d526a
leizq123 <66864066+leizq123@users.noreply.github.com>
2022-03-26 10:20:16 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,10 +1,8 @@
-
 document.addEventListener("DOMContentLoaded", function(event) {
 alert("Hello JavaScript");
 let targetImage = document.querySelector("#smart-image");
  targetImage.addEventListener("click", function() 
  {
-   targetImage.classList.remove("small");
+   alert("图片");
   });
- 
-});
+ });
