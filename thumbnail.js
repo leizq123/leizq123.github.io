fd81bad84caaf94c8bc1f4b56e5c401b5481454c
leizq123 <66864066+leizq123@users.noreply.github.com>
2022-03-26 10:13:47 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -2,12 +2,14 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 alert("Hello JavaScript");
 let targetImage = document.querySelector("#smart-image");
- targetImage.addEventListener("click", function() {
-   if (targetImage.classList.contains("small")) {
+ targetImage.addEventListener("click", function() 
+ {
+   if (targetImage.classList.contains("small")) 
+     {
    targetImage.classList.remove("small");
-} else {
+   } else {
    targetImage.classList.add("small");
-} 
-});
+   } 
+  });
  
 });
